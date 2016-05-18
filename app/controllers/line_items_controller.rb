31ecf66ac2781434582557a0d8979f8ce0936ace
Bart De Nef <bartdn183@gmail.com>
2016-05-18 10:10:39 -0400

    deleted json files part2

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -50,7 +50,6 @@ class LineItemsController < ApplicationController
     @line_item.destroy
     respond_to do |format|
       format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -48,7 +48,6 @@ class OrdersController < ApplicationController
     @order.destroy
     respond_to do |format|
       format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
